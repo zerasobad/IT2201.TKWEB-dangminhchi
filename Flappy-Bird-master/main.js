50bdab98f97c27feb4006e23c4aadc6d621c5375
DangMinhChi <dgminhchi.work@gmail.com>
2023-07-28 16:32:59 +0700

    Update main.js

--- Flappy-Bird-master/main.js
+++ Flappy-Bird-master/main.js
@@ -82,7 +82,7 @@ function run(){
 }
 //thêm function cho nó bay lên khi nhấn
 document.addEventListener("click",function(){
-    bird.y-=60;   
+    bird.y-=90;   
 })
 // ok khá ổn 
 // các bạn nhớ là tọa độ trên máy tính là ở gốc trên trái đi xuống dưới 
