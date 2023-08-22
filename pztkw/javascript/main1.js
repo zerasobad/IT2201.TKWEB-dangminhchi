c336e2092ff6fe7c7d3a48004b9be16e6a45e5f4
DangMinhChi <dgminhchi.work@gmail.com>
2023-08-22 09:59:34 +0700

    a
    a

--- pztkw/javascript/main1.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector('#menuIcon');
-let navbar = document.querySelector('.navbar');
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
