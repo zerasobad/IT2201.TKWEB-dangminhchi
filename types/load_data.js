9e707eac806f04de9585cf23390be86f84c8c550
DangMinhChi <dgminhchi.work@gmail.com>
2023-08-23 22:13:25 +0700

    a
    a

--- types/load_data.js
+++ types/load_data.js
@@ -66,4 +66,26 @@ fetch("data.json").then(res => res.json()).then(data => {
         `
         List.appendChild(newli);
     }
+    List = document.getElementById('listduaxe');
+    for(let a of data.duaxe){
+        let newli = document.createElement('li');
+        newli.classList.add("item");
+        newli.innerHTML=`
+        <div class="box"> 
+            <img src = "${a.src}">
+            <div class="text">
+            <h1>${a.name}</h1>
+            <h2><i class="fa-solid fa-gamepad"></i>${a.count}</h2>
+            <p>
+            <i class="fa-${a.star1} fa-star"></i>
+            <i class="fa-${a.star2} fa-star"></i>
+            <i class="fa-${a.star3} fa-star"></i>
+            <i class="fa-${a.star4} fa-star"></i>
+            <i class="fa-${a.star5} fa-star"></i>
+            </p>
+            </div>
+        </div>
+        `
+        List.appendChild(newli);
+    }
 })
--- types/main.js
+++ types/main.js
@@ -77,7 +77,6 @@ setInterval(imgAuto,5000);
 /*-----------------------------------BTN---------------- */
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -88,7 +87,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
@@ -132,3 +130,12 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+// switch-mode
+  const body= document.body;
+    const switch_mode=document.querySelector('#switch-mode i');
+    switch_mode.addEventListener('click', () =>{
+        body.classList.toggle('dark');
+        switch_mode.classList.toggle('bi-bi-brightness-high-fill');
+        switch_mode.classList.toggle('bi-bi-moon-stars-fill');
+    });
